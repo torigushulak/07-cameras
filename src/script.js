bbc7758a42a318be2a6939fde23c1c34106468a6
Tori Gushulak <tori.gushulak@gmail.com>
2023-04-13 22:08:20 -0600

    cameras done

--- src/script.js
+++ src/script.js
@@ -1,4 +1,17 @@
 import * as THREE from 'three'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+
+
+// cursor
+const cursor = {
+    x: 0,
+    y:0
+}
+ window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width -0.5;
+    cursor.y = -(event.clientY / sizes.height -0.5);
+    // console.log(cursor.y)
+ })
 
 /**
  * Base
@@ -23,12 +36,22 @@ const mesh = new THREE.Mesh(
 scene.add(mesh)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+
+// const aspectRatio = sizes.width / sizes.height; 
+// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100)
+
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
+
+// controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true;
+// controls.target.y = 1
+// controls.update()
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -44,7 +67,16 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    mesh.rotation.y = elapsedTime;
+    // mesh.rotation.y = elapsedTime;
+
+    // update camera
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2)* 3;
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2)* 3;
+    // camera.position.y = cursor.y * 5;
+    // camera.lookAt(mesh.position)
+
+    // update controls
+    controls.update()
 
     // Render
     renderer.render(scene, camera)
